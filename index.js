799836def58e170bca4f7b42e9cf925295c574e5
Rubi Alvarado <kimjeonrubi7@gmail.com>
2024-03-21 15:40:30 -0600

    Repo Prueba

---
+++ index.js
@@ -0,0 +1,6 @@
+const hola = () => {
+    console.log(
+        'Buena tarde, gracias por estar al pendiente. El día de ayer fui a traerlo, y hoy, cuando me llamaron, estaba en el banco. Intenté devolver la llamada pero no pude contactarla. Ya realicé los pagos de mis antecedentes. El día de hoy estaré tramitándolos."');
+}
+
+hola()
